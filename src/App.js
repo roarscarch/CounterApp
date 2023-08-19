f7da9566b6325ce1cef5a101f3622c73a69eadb8
roarscarch <2020ugee082@nitjsr.ac.in>
2023-08-20 01:58:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { useState } from 'react';
 function App() {
+  function increaseCounter() {
+    setCounter(counter+1);
+  }
+  function decreaseCounter() {
+    setCounter(counter-1);
+  }
+  
+  let [counter,setCounter] = useState(0);
+  console.log("anurage");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={increaseCounter}>+</button>
+      <span>{counter}</span>
+      <button onClick={decreaseCounter}>-</button>
+     
+      
     </div>
   );
 }
